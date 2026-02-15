0c67329c516bf7821d76a1d2e4289bfa9630b1e8
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-02-15 09:58:12 +0530

    feat: add TrackExecutionTime annotation and execution time aspect

---
+++ src/main/java/com/sathwikhbhat/apiexecutiontracker/annotation/TrackExecutionTime.java
@@ -0,0 +1,9 @@
+package com.sathwikhbhat.apiexecutiontracker.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TrackExecutionTime {
+}
---
+++ src/main/java/com/sathwikhbhat/apiexecutiontracker/aspect/ExecutionTimeAspect.java
@@ -0,0 +1,31 @@
+package com.sathwikhbhat.apiexecutiontracker.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExecutionTimeAspect.class);
+
+    @Around("@within(com.sathwikhbhat.apiexecutiontracker.annotation.TrackExecutionTime) || " +
+            "@annotation(com.sathwikhbhat.apiexecutiontracker.annotation.TrackExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.nanoTime();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long end = System.nanoTime();
+            long durationMs = (end - start) / 1_000_000;
+
+            if (logger.isInfoEnabled()) {
+                logger.info("{} executed in {} ms", joinPoint.getSignature().toShortString(), durationMs);
+            }
+        }
+    }
+}
